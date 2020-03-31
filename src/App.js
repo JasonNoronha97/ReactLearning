370c207cd6a59939b3cea2a1d0d751c64093afc7
Jason Noronha <noronha.jason97@gmail.com>
2020-03-31 12:58:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';    //Custom Component
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent"; //Custom Component
 import FirstComponent from "./components/FirstComponent"; //created by me.
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-          <FirstComponent displaytext="Jason Noronha"/>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+import { DISHES } from "./shared/dishes.js";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <FirstComponent displaytext="Jason Noronha" />
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
